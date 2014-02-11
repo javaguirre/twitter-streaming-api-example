a1ac95d469578a58cfaaac92612f5a451ee58dea
Javier Aguirre <contacto@javaguirre.net>
2014-02-11 15:48:26 +0100

    Streaming tweets from ReactJS

--- public/javascripts/fight.js
+++ public/javascripts/fight.js
@@ -1,13 +1,61 @@
 /** @jsx React.DOM */
-// connect to the socket server
-var socket = io.connect();
+var Tweet = React.createClass({
+    render: function() {
+        return (
+            <li>{this.props.text}</li>
+        )
+    }
+});
 
-// if we get an "info" emit from the socket server then console.log the data we recive
-socket.on('info', function (data) {
-    console.log(data);
+var TweetList = React.createClass({
+    render: function() {
+        var tweets = this.props.data.map(function(tweet) {
+            return <Tweet text={tweet.text} />;
+        });
+        return (
+            <div>
+                <ul>
+                    {tweets}
+                </ul>
+            </div>
+        )
+    }
+});
+
+var TweetBox = React.createClass({
+    addTweet: function(tweet) {
+        var tweets = this.state.data;
+        var newTweets = tweets.concat([tweet]);
+
+        if(newTweets.length > 15) {
+            newTweets.splice(0, 1);
+        }
+
+        this.setState({data: newTweets});
+    },
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentWillMount: function() {
+        var socket = io.connect();
+        var self = this;
+
+        socket.on('info', function (data) {
+            self.addTweet(data.tweet);
+        });
+    },
+    render: function() {
+        return (
+            <div>
+                <h1>Hello</h1>
+
+                <TweetList data={this.state.data} />
+            </div>
+        )
+    }
 });
 
 React.renderComponent(
-  <h1>Hello, world!</h1>,
+  <TweetBox />,
   document.getElementById('content')
 );
